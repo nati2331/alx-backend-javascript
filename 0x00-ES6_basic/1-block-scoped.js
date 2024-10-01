8bfc36857002c9357ec5bf406f038871c5a0d77d
nati2331 <natnael.mk2@gmail.com>
2024-10-01 10:59:52 +0000

    fixed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,3 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-  }
-
-  return [task, task2];
+  return trueOrFalse ? [true, false] : [false, true];
 }
