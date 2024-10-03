33de64e87ac45203a04ba44454cf9dfbd52ffea8
nati2331 <natnael.mk2@gmail.com>
2024-10-03 22:29:30 +0000

    updates

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,13 @@
 export default class Building {
-  // Constructor initializes sqft
   constructor(sqft) {
-    this._sqft = sqft;  // Store the sqft in a private attribute
+    this._sqft = sqft;
   }
 
-  // Getter for sqft
   get sqft() {
-    return this._sqft;  // Return the stored sqft
+    return this._sqft;
   }
 
-  // Method to enforce the implementation of evacuationWarningMessage
   evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');  // Throw error if not overridden
+    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
