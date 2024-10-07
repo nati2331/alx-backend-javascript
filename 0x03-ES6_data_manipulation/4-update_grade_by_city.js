5b0b29c0a08c8c4823181cffed264a490ba20642
nati2331 <natnael.mk2@gmail.com>
2024-10-07 16:45:53 +0000

    second try at question 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,17 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const defaultGrade = 'N/A';
+  const defaultGrade = { grade: 'N/A' };
 
   if (Array.isArray(students)) {
     return students
-      .filter(student => student.location === city)
-      .map(student => {
-        const grade = newGrades.find(grade => grade.studentId === student.id) || { grade: defaultGrade };
-        
-        return {
-          id: student.id,
-          firstName: student.firstName,
-          location: student.location,
-          grade: grade.grade,
-        };
-      });
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (newGrades
+          .filter((grade) => grade.studentId === student.id)
+          .pop() || defaultGrade).grade,
+      }));
   }
   return [];
 }
