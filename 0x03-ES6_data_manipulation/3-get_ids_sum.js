83933a2806d1070ff7c235536a70ee970640f82e
nati2331 <natnael.mk2@gmail.com>
2024-10-07 16:37:30 +0000

    updated question 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,14 +1,9 @@
-/**
- * getStudentIdsSum - Returns the sum of all student IDs from a list of student objects.
- * @param {Array} students - Array of student objects.
- * @returns {Number} - Sum of student IDs.
- */
-const getStudentIdsSum = (students) => {
-  if (!Array.isArray(students)) {
-    return 0;
+export default function getStudentIdsSum(students) {
+  if (Array.isArray(students)) {
+    return students.reduce(
+      (accumulator, currentStudent) => accumulator + currentStudent.id,
+      0,
+    );
   }
-  
-  return students.reduce((accumulator, student) => accumulator + student.id, 0);
-};
-
-export default getStudentIdsSum;
+  return 0;
+}
