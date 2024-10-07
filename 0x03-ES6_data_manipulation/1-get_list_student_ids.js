66b158293202e8a3eb54a4fb454792f0ff0a4fc8
nati2331 <natnael.mk2@gmail.com>
2024-10-07 16:32:31 +0000

    updated question 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,14 +1,6 @@
-/**
- * getListStudentIds - Returns an array of student IDs from a list of student objects.
- * @param {Array} students - Array of student objects.
- * @returns {Array} - Array of student IDs.
- */
-const getListStudentIds = (students) => {
-  if (!Array.isArray(students)) {
-    return [];
+export default function getListStudentIds(students) {
+  if (Array.isArray(students)) {
+    return students.map((student) => student.id);
   }
-  
-  return students.map(student => student.id);
-};
-
-export default getListStudentIds;
+  return [];
+}
